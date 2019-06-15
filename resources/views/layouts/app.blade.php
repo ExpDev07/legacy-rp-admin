55bfa212f2ae6081f9c71bfb3bba79f43455d87f
sammikinsrox <samanthahldr@gmail.com>
2019-06-15 19:23:19 -0400

    Fixed header and deleted bright backgrounds

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <?php
+    $bg = array('bg01.jpg', 'bg03.jpg', 'bg04.jpg', 'bg05.jpg', 'bg06.jpg', 'bg08.jpg', 'bg011.jpg', 'bg012.jpg', 'bg013.jpg', 'bg015.jpg', 'bg016.jpg', 'bg017.jpg', 'bg018.jpg', 'bg019.jpg', 'bg020.png' ); // array of filenames
+
+    $i = rand(0, count($bg)-1); // generate random number size of the array
+    $selectedBg = "$bg[$i]"; // set variable equal to which random filename was chosen
+    ?>
+        <style type="text/css">
+            <!-- body {
+                background: url(img/<?php echo $selectedBg; ?>) center;
+                font-family: 'Roboto Condensed', sans-serif;
+                background-attachment: fixed;
+            }
+
+            -->
+        </style>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -23,11 +38,12 @@
         <header>
             <div class="container">
                 <div class="row">
-                    <div class="col-sm-3"></div>
-                    <div class="col-sm-6">
-                        <img src="./img/logo.png" class="img-fluid py-3">
-                    </div>
-                    <div class="col-sm-3"></div>
+                    <div class="col-sm-2 py-2"><img src="./img/logo.png" class="img-fluid py-2"></div>
+                    <div class="col-sm-2 py-5"><a href="#" class="nav-main">About LegacyRP</a></div>
+                    <div class="col-sm-2 py-5"><a href="#" class="nav-main">LegacyRP Rules</a></div>
+                    <div class="col-sm-2 py-5"><a href="#" class="nav-main">How to Play</a></div>
+                    <div class="col-sm-2 py-5"><a href="#" class="nav-main">City Forums</a></div>
+                    <div class="col-sm-2 py-5"><a href="#" class="nav-main">Player Administration</a></div>
                 </div>
             </div>
         </header>
