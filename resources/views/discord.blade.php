48b9f38f086c68eb2c59f1eef9ca6991e1659a48
sammikinsrox <samanthahldr@gmail.com>
2019-06-16 03:31:35 -0400

    finished the discord page

--- resources/views/discord.blade.php
+++ resources/views/discord.blade.php
@@ -4,12 +4,17 @@
     <div class="container">
         <!--This is the header text-->
         <div class="row">
-            <div class="col-sm-12">
-                <div class="alert alert-info box-round" role="alert">
-                    <p>Welcome to the LegacyRP Administration Section. This is a placeholder section</p>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-8">
+                <div class="alert alert-secondary lead text-center" role="alert"><a
+                        href="https://discordapp.com/invite/6j2uEjj">Click Here</a> to join the Discord
                 </div>
+                <center>
+                    <iframe src="https://discordapp.com/widget?id=407992059430043649&theme=dark" width="100%"
+                            height="400px" allowtransparency="true" frameborder="0"></iframe>
+                </center>
             </div>
+            <div class="col-sm-2"></div>
         </div>
     </div>
-
 @endsection
