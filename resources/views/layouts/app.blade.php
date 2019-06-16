e9f343101533e6e0e7b9742e7c9a4121855a8769
ExpDev07 <mariusrich2@gmail.com>
2019-06-16 02:52:50 +0200

    fixed kate missing closing tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,14 +6,13 @@
     $bg = array('bg01.jpg', 'bg03.jpg', 'bg04.jpg', 'bg06.jpg', 'bg011.jpg', 'bg012.jpg', 'bg013.jpg', 'bg015.jpg', 'bg016.jpg', 'bg017.jpg', 'bg018.jpg', 'bg019.jpg', 'bg020.png' ); // array of filenames
 
     $i = rand(0, count($bg)-1); // generate random number size of the array
-    $selectedBg = "$bg[$i]"; // set variable equal to which random filename was chosen
+    $selectedBg = $bg[$i]; // set variable equal to which random filename was chosen
     ?>
     <style type="text/css">
         <!-- body {
-            background: url(img/<?php echo $selectedBg; ?>) center;
+            background: url("img/<?php echo $selectedBg; ?>") center fixed;
             font-family: 'Roboto Condensed',
             sans-serif;
-            background-attachment: fixed;
         }
         -->
     </style>
@@ -41,30 +40,42 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-2 py-2"><img src="./img/logo.png" class="img-fluid py-2" alt="Legacy RP"></div>
-                    <div class="col-sm-10 py-3">
-                        <div class="row">
-                            <div class="col-sm-2"><p><a href="#" class="nav-main" alt="About Legacy RP">About LegacyRP</p><p class="nav-desc d-none d-sm-block">More information about legacy RP and our community</p></a></div>
-                            <div class="col-sm-2"><p><a href="#" class="nav-main" alt="Legacy RP Rules">LegacyRP Rules</p><p class="nav-desc d-none d-sm-block">Learn the city rules and how to play on our community</p></a></div>
-                            <div class="col-sm-2"><p><a href="#" class="nav-main" alt="Rules and How to Play">How to Play</p><p class="nav-desc d-none d-sm-block">Learn about commands, jobs, and get a printable cheat sheet</p></a></div>
-                            <div class="col-sm-2"><p><a href="#" class="nav-main" alt="City Forums">City Forums</p><p class="nav-desc d-none d-sm-block">Get help and make suggestions in our city forums</p></a></div>
-                            <div class="col-sm-2"><p><a href="#" class="nav-main" alt="City Discord">City Discord</p><p class="nav-desc d-none d-sm-block">Join in the discussion with thousands of citizens in our Discord</p></a></div>
-                            <div class="col-sm-2">
-                                <!-- Display information based on if user is authenticated or not -->
+                <div class="col-sm-10 py-3">
+                    <div class="row">
+                        <div class="col-sm-2">
+                            <a href="#" class="nav-main">About LegacyRP</p><p class="nav-desc d-none d-sm-block">More information about legacy RP and our community</p></a>
+                        </div>
+                        <div class="col-sm-2">
+                            <a href="#" class="nav-main">LegacyRP Rules</p><p class="nav-desc d-none d-sm-block">Learn the city rules and how to play on our community</p></a>
+                        </div>
+                        <div class="col-sm-2">
+                            <a href="#" class="nav-main">How to Play</p><p class="nav-desc d-none d-sm-block">Learn about commands, jobs, and get a printable cheat sheet</p></a>
+                        </div>
+                        <div class="col-sm-2">
+                            <a href="#" class="nav-main">City Forums</p><p class="nav-desc d-none d-sm-block">Get help and make suggestions in our city forums</p></a>
+                        </div>
+                        <div class="col-sm-2">
+                            <a href="#" class="nav-main">City Discord<p class="nav-desc d-none d-sm-block">Join in the discussion with thousands of citizens in our Discord</p></a>
+                        </div>
+                        <div class="col-sm-2">
+                            <!-- Display information based on if user is authenticated or not -->
                             @if (Auth::check())
                                 <!-- Display some basic user information -->
-                                    <img class="img-fluid rounded-circle d-none d-sm-block" style="max-height:40px;" src="{{Auth::user()->avatar}}" alt="Profile Image">
-                                    <p class="nav-desc d-none d-sm-block">{{ Auth::user()->username }}</p>
+                                <img class="img-fluid rounded-circle d-none d-sm-block" style="max-height:40px;" src="{{Auth::user()->avatar}}" alt="Profile Image">
+                                <p class="nav-desc d-none d-sm-block">{{ Auth::user()->username }}</p>
 
-                                    <!-- Display a logout button -->
-                                    <a href="{{route('logout')}}" class="nav-main">Logout</a>
+                                <!-- Display a logout button -->
+                                <a href="{{route('logout')}}" class="nav-main">Logout</a>
                             @else
                                 <!-- Display a login with steam button -->
-                                    <a href="{{route('auth.steam')}}" class="nav-main">Login with Steam</a></p>
-                                    <p class="nav-desc">Login with steam to (I can't think of a description)</p></div>
-                                @endif
+                                <a href="{{route('auth.steam')}}" class="nav-main">Login with Steam</a>
+                                <p class="nav-desc">Login with steam to (I can't think of a description)</p>
+                            @endif
                         </div>
                     </div>
+                </div>
             </div>
+        </div>
     </header>
 
     <!-- Displaying of content -->
