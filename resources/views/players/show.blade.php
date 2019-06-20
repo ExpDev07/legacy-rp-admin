8127db24d9d292633f5e98e5caa2da3f6e0f0cff
ExpDev07 <mariusrich2@gmail.com>
2019-06-20 02:24:32 +0200

    Fixed.

--- resources/views/players/show.blade.php
+++ resources/views/players/show.blade.php
@@ -1,4 +1,6 @@
-@extends('layouts.app') @section('content')
+@extends('layouts.app')
+
+@section('content')
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
